c7c78ba0db6b080c88c55dea645967583365041a
Arsen <arsen.sheripov@gmail.com>
2026-02-28 20:07:43 +0300

    base configuration

---
+++ src/app/providers/router/index.tsx
@@ -0,0 +1,9 @@
+import { createBrowserRouter } from "react-router";
+import { AuthPage } from "@pages/auth-page";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AuthPage />,
+  }
+])
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@app': path.resolve(__dirname, './src/app'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@shared': path.resolve(__dirname, './src/shared'),
+      '@widgets': path.resolve(__dirname, './src/widgets'),
+      '@entities': path.resolve(__dirname, './src/entities'),
+      '@features': path.resolve(__dirname, './src/features'),
+    }
+  },
+
 })
